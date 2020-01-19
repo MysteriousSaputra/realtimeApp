10f9fd23bdf1e3c13ae4c38ca45da5abb2e18985
MysteriousSaputra <ilham05saputra@gmail.com>
2020-01-19 22:23:23 +0700

    JWT Middleware and Exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,10 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenBlacklistedException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
 
 class Handler extends ExceptionHandler
 {
@@ -46,6 +50,16 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($exception instanceof TokenBlacklistedException) {
+            return response('token can not be used', 400);
+        } else if ($exception instanceof TokenInvalidException) {
+            return response('token is invalid', 400);
+        } else if ($exception instanceof TokenExpiredException) {
+            return response('token is expired', 400);
+        } else if ($exception instanceof JWTException) {
+            return response('token is not provide', 400);
+        }
+
         return parent::render($request, $exception);
     }
 }
--- app/Http/Controllers/QuetionController.php
+++ app/Http/Controllers/QuetionController.php
@@ -9,6 +9,16 @@ use App\Http\Resources\QuetionResource;
 class QuetionController extends Controller
 {
     /**
+     * Create a new AuthController instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('JWT', ['except' => ['index', 'show']]);
+    }
+
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
