7bf97aa8e272c1b6b04d60f72ae2ff0145b426ab
MysteriousSaputra <ilham05saputra@gmail.com>
2020-01-19 21:02:29 +0700

    JWT Package

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,17 @@ Route::apiResource('quetion/{quetion}/reply', 'ReplyController');
 
 Route::post('like/{reply}', 'LikeController@likeIt');
 Route::delete('like/{reply}', 'LikeController@unLikeIt');
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
